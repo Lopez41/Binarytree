8640c1b984c15592b63707ce4978d5ea70fb04d2
Adrian Lopez <adrianlopez@Adrians-Air.attlocal.net>
2023-07-13 19:56:59 -0500

    added constructor,destructor and public method isEmpty()

--- bintree.cpp
+++ bintree.cpp
@@ -1,7 +1,31 @@
-/*
- * Your comment header here
- */
+/*******************
+*coding assignment 08: Binary tree
+*Name: Adrian Lopez
+*Date: 07/13/23
+*Status:in progress
+*Language: C++
+*File: bintree.cpp
+*******************/
+
 
 #include "bintree.h"
 
+// Constructor
+BinTree::BinTree() 
+{
+    root = nullptr;
+    
+}
+
+// Destructor
+BinTree::~BinTree() 
+{
+    clear();
+}
+
+// Public method: isEmpty
+bool BinTree::isEmpty() 
+{
+    return (count == 0);
+}
 
