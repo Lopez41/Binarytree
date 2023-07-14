5ee8c648c90848c6d1b9ef29bafb69f7699867db
Adrian Lopez <adrianlopez@Adrians-Air.attlocal.net>
2023-07-13 19:06:13 -0500

    fixed intCount() and class typo mistake and added using std for cout and endl and added iostream

--- bintree.h
+++ bintree.h
@@ -11,35 +11,51 @@
 #define BINTREE_BINTREE_H
 
 #include "data.h"
+#include <iostream>
+using std:: cout;
+using std:: endl;
 
-clas BinTree
+class BinTree
 {
 public:
 
-//constructor
- Bintree();
+    //constructor
+    BinTree();
 
-//destructor
- ~BinTree();
+    //destructor
+    ~BinTree();
 
-//public methods
- 
- bool isEmpty();
- int getCount():
- bool getRootData(Data*);
- void displayTree();
- void clear();
- bool addNode(int,const string*);
- bool removeNode(int);
- bool getNode(Data*,int);
- bool contains(int);
- int getHeight();
- void displayPreOrder();
- void displayPostOrder();
- void displayInOrder();
+    //public methods
+    
+    bool isEmpty();
+    int getCount();
+    bool getRootData(Data*);
+    void displayTree();
+    void clear();
+    bool addNode(int,const string*);
+    bool removeNode(int);
+    bool getNode(Data*,int);
+    bool contains(int);
+    int getHeight();
+    void displayPreOrder();
+    void displayPostOrder();
+    void displayInOrder();
+    
+private:
+    DataNode *root; // Pointer to the root node of the binary search tree
+    int count;      // Number of nodes in the binary search tree
 
-
-}
+    // Private recursive methods
+    void clear(DataNode*);
+    bool addNode(DataNode*, DataNode**);
+    DataNode* removeNode(int, DataNode*);
+    bool getNode(Data*, int, DataNode*);
+    bool contains(int, DataNode*);
+    int getHeight(DataNode*);
+    void displayPreOrder(DataNode*);
+    void displayPostOrder(DataNode*);
+    void displayInOrder(DataNode*);
+};
 
 #endif /* BINTREE_BINTREE_H */
 
